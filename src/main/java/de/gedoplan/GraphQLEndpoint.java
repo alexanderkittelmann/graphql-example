2c56a79d22d24f99139fa4bf53e6bef1a43c4b94
akittelmann <alexander.kittelmann@gedoplan.de>
2018-11-09 18:56:33 +0100

    init

---
+++ src/main/java/de/gedoplan/GraphQLEndpoint.java
@@ -0,0 +1,26 @@
+package de.gedoplan;
+
+import com.coxautodev.graphql.tools.SchemaParser;
+import graphql.schema.GraphQLSchema;
+import graphql.servlet.SimpleGraphQLServlet;
+
+import javax.servlet.annotation.WebServlet;
+
+
+@WebServlet(urlPatterns = "/graphql")
+public class GraphQLEndpoint extends SimpleGraphQLServlet {
+
+    public GraphQLEndpoint() {
+        super(buildSchema());
+    }
+
+    public static GraphQLSchema buildSchema() {
+        LinkRepository linkRepository = new LinkRepository();
+        return SchemaParser.newParser()
+                .file("schema.graphqls")
+                .resolvers(new Query(linkRepository))
+                .build()
+                .makeExecutableSchema();
+    }
+}
+
