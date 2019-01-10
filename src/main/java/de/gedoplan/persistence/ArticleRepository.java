c08da4e536c5e4fba500f9eea72043c02722268f
akittelmann <alexander.kittelmann@gedoplan.de>
2019-01-10 13:55:24 +0100

    Repositorymethode fuer einzelne Article ergänzt

--- src/main/java/de/gedoplan/persistence/ArticleRepository.java
+++ src/main/java/de/gedoplan/persistence/ArticleRepository.java
@@ -1,15 +1,16 @@
 package de.gedoplan.persistence;
 
-import de.gedoplan.model.Article;
-import io.leangen.graphql.annotations.GraphQLArgument;
-import io.leangen.graphql.annotations.GraphQLMutation;
-import io.leangen.graphql.annotations.GraphQLQuery;
+import java.util.List;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
-import java.util.List;
+
+import de.gedoplan.model.Article;
+import io.leangen.graphql.annotations.GraphQLArgument;
+import io.leangen.graphql.annotations.GraphQLMutation;
+import io.leangen.graphql.annotations.GraphQLQuery;
 
 @RequestScoped
 public class ArticleRepository {
@@ -18,14 +19,19 @@ public class ArticleRepository {
     private EntityManager entityManager;
 
     @GraphQLQuery(name = "articles")
-    public List<Article> getAllCustomers() {
+    public List<Article> getAllArticles() {
         return this.entityManager.createQuery("select a from Article a", Article.class)
                 .getResultList();
+    }
+    
+    @GraphQLQuery(name = "article")
+    public Article getArticle(@GraphQLArgument(name = "id") Long id) {
+        return this.entityManager.find(Article.class, id);
     }
 
     @Transactional
     @GraphQLMutation(name = "createarticle")
-    public Article persistCustomer(@GraphQLArgument(name = "article") Article article) {
+    public Article persistArticle(@GraphQLArgument(name = "article") Article article) {
         this.entityManager.persist(article);
         return article;
     }
